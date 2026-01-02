f89011bb817f6f87a7eda3f3a8118f83fced3a5f
Cavin-xyz <cavinxp@gmail.com>
2026-01-02 11:08:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static int isEven(int n) {
+        if (n % 2 == 0)
+            return 2;
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {}
+    
+}
+
